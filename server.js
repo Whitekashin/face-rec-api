30443ab0c1404b6a67aeff47a9311b8f6371b409
Whitekashin <patrick.reitbauer@web.de>
2022-01-04 03:51:40 +0100

    updating database

--- server.js
+++ server.js
@@ -13,9 +13,9 @@ const image = require("./controllers/image.js");
 const db = knex({
   client: "pg",
   connection: {
-    host: "127.0.0.1",
-    user: "postgres",
-    password: "GH0STWR4TH",
+    host: "postgresql-clean-43205",
+    user: "Patrick Reitbauer",
+    password: "",
     database: "smart-brain",
   },
 });
